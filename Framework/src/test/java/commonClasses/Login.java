3baef078a8445075650aee5d0845d10bcba7a844
vijayshire <vijayhire@rediffmail.com>
2018-11-24 17:01:18 +0530

    1.4

--- Framework/src/test/java/commonClasses/Login.java
+++ Framework/src/test/java/commonClasses/Login.java
@@ -2,66 +2,63 @@ package commonClasses;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import testCases.NewTest;
 
 public class Login {
-	
-	
-	public static boolean txtBoxUserName(WebDriver driver,String userName) {
-		
-		try {
-		
-		if (driver.findElement(By.xpath("html/body/div[2]/div/div/div/div/div[2]")).isEnabled()) {
 
-			driver.findElement(By.xpath("html/body/div[2]/div/div/button")).click();
+	public static boolean txtBoxUserName(WebDriver driver, String userName) {
+
+		try {
+
+			if (driver.findElement(By.xpath("html/body/div[2]/div/div/div/div/div[2]")).isEnabled()) {
+
+				driver.findElement(By.xpath("html/body/div[2]/div/div/button")).click();
+			}
+
+			driver.findElement(By.xpath(".//*[@id='container']/div/header/div[1]/div/div[3]/div[1]/div/a")).click();
+
+			driver.findElement(By.xpath("html/body/div[2]/div/div/div/div/div[2]/div/form/div[1]/input"))
+					.sendKeys(userName);
+
+			return true;
+
 		}
 
-		driver.findElement(By.xpath(".//*[@id='container']/div/header/div[1]/div/div[3]/div[1]/div/a")).click();
+		catch (Exception e) {
 
-		driver.findElement(By.xpath("html/body/div[2]/div/div/div/div/div[2]/div/form/div[1]/input"))
-				.sendKeys(userName);
-		
-		return true;
-		
-		}		
-		
-		catch(Exception e) {
-			
-			System.out.println("Error Occurred"+e);
+			System.out.println("Error Occurred" + e);
 			return false;
 		}
-	}  
+	}
 
-	public static boolean txtBoxPassword(WebDriver driver,String password) {
-		
-		
+	public static boolean txtBoxPassword(WebDriver driver, String password) {
+
 		try {
-			
+
 			driver.findElement(By.xpath("html/body/div[2]/div/div/div/div/div[2]/div/form/div[2]/input"))
-			.sendKeys(password);
+					.sendKeys(password);
 			return true;
 		}
-		
-		catch(Exception e) {
-			
-			System.out.println("Error Occurred"+e);
+
+		catch (Exception e) {
+
+			System.out.println("Error Occurred" + e);
 			return false;
 		}
-		
+
 	}
-	
+
 	public static boolean btnLogin(WebDriver driver) {
-		
+
 		try {
-			
+
 			driver.findElement(By.xpath("html/body/div[2]/div/div/div/div/div[2]/div/form/div[3]/button")).click();
 			return true;
 		}
-		
-		catch(Exception e) {
-			System.out.println("Error Occurred"+e);
+
+		catch (Exception e) {
+			System.out.println("Error Occurred" + e);
 			return false;
-			
+
 		}
 	}
 }
